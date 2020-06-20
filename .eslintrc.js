1375f121ce0ae2d914db60c7ae69e9aade34279b
William Cowell <williamcowell@Williams-MBP.lan>
2020-06-20 15:17:17 +0100

    #card-40437988 Converted to TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  'env': {
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'no-unused-vars': 0,
+    'semi': ['error', 'never'],
+    'require-jsdoc': 0,
+    'indent': ['error', 4]
+  },
+};
